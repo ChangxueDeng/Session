fb8404a51118ca942fb8e340d7b777a0c36e82d6
changxuedeng <dengchangxue@qq.com>
2024-01-17 14:31:11 +0800

    首次创建

---
+++ session-backdend/src/test/java/org/example/sessionbackdend/SessionBackdendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sessionbackdend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionBackdendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
